88c493229575fe69f487c80ae0af634b3ed9e3ce
sakamitz <sakamitz@yahoo.co.jp>
2019-05-19 20:44:25 +0800

    Memory leak

---
+++ laziness-of-ghc/src/map-negate.hs
@@ -0,0 +1,5 @@
+map negate [1,2,3]
+<thunk: map negate <thunk: (1:2:3:[])>>
+<thunk: negate <thunk: 1> : <thunk: map negate <thunk: [2,3]>>
+-<thunk: 1> : <thunk: map negate <thunk: [2,3]>>
+-1 : <thunk: map negate <thunk: [2,3]>>
